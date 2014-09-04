85e7898476b3798665ab52145526dae010b1e8e7
Darren Cauthon <darren@cauthon.com>
2014-09-04 15:02:44 -0500

    Getting closer.

--- lib/seam/sidekiq.rb
+++ lib/seam/sidekiq.rb
@@ -8,6 +8,10 @@ module Seam
 
         def perform effort_id
           effort = Seam::Effort.find effort_id
+          next_worker = Seam::Worker.handler_for effort.next_step
+          if next_worker
+            next_worker.class.perform_async effort.id
+          end
           execute effort
         end
       end
--- test_app/spec/test_situation_spec.rb
+++ test_app/spec/test_situation_spec.rb
@@ -9,14 +9,54 @@ describe "testing the sidekiq integration into seam" do
   end
 
   describe "the perform method" do
-    it "should look up the effort, then execute it" do
-      effort, effort_id = Object.new, Object.new
-      Seam::Effort.stubs(:find).with(effort_id).returns effort
 
-      worker = SendEmailWorker.new
+    let(:effort_id) { Object.new }
+
+    let(:effort) do
+      Struct.new(:id, :next_step).new effort_id, Object.new
+    end
+
+    let(:worker) do
+      w = SendEmailWorker.new
+      w.stubs(:execute)
+      w
+    end
+
+    let(:next_worker_class) do
+      c = Object.new
+      c.stubs(:perform_async)
+      c.stubs(:perform_in)
+      c
+    end
+
+    let(:next_worker) do
+      w = Object.new
+      w.stubs(:class).returns next_worker_class
+      w
+    end
+
+    before do
+      Seam::Effort.stubs(:find).with(effort_id).returns effort
+      Seam::Worker.stubs(:handler_for).returns nil
+    end
+
+    it "should look up the effort, then execute it" do
       worker.expects(:execute).with effort
 
       worker.perform effort_id
     end
+
+    describe "and there is a next step to execute" do
+
+      before do
+        Seam::Worker.stubs(:handler_for).with(effort.next_step).returns next_worker
+      end
+
+      it "should pass it to sidekiq" do
+        next_worker_class.expects(:perform_async).with effort_id
+        worker.perform effort_id
+      end
+    end
+
   end
 end
