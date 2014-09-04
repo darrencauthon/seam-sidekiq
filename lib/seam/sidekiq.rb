d41c0dab22facae2b0e69feb2a5dd5d0ac8a54d0
Darren Cauthon <darren@cauthon.com>
2014-09-04 14:28:51 -0500

    The sidekiq perform should execute the work.

--- lib/seam/sidekiq.rb
+++ lib/seam/sidekiq.rb
@@ -5,6 +5,11 @@ module Seam
     def self.setup
       ::Seam::Worker.class_eval do
         include ::Sidekiq::Worker
+
+        def perform effort_id
+          effort = Seam::Effort.find effort_id
+          execute effort
+        end
       end
     end
   end
--- test_app/spec/test_situation_spec.rb
+++ test_app/spec/test_situation_spec.rb
@@ -7,4 +7,16 @@ describe "testing the sidekiq integration into seam" do
   it "should make the worker a sidekiq worker" do
     SendEmailWorker.new.is_a?(Sidekiq::Worker).must_equal true
   end
+
+  describe "the perform method" do
+    it "should look up the effort, then execute it" do
+      effort, effort_id = Object.new, Object.new
+      Seam::Effort.stubs(:find).with(effort_id).returns effort
+
+      worker = SendEmailWorker.new
+      worker.expects(:execute).with effort
+
+      worker.perform effort_id
+    end
+  end
 end
