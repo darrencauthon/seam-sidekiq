f2583da8edf43d1aa6ee41f4f66b931d5757c644
Darren Cauthon <darren@cauthon.com>
2014-09-04 20:31:35 -0500

    Refactor.

--- lib/seam/sidekiq.rb
+++ lib/seam/sidekiq.rb
@@ -9,14 +9,18 @@ module Seam
         def perform effort_id
           effort = Seam::Effort.find effort_id
           execute effort
-          next_worker = Seam::Worker.handler_for effort.next_step
-          if next_worker
-            if effort.next_execute_at <= Time.now
-              next_worker.class.perform_async effort.id
-            else
-              next_worker.class.perform_in effort.next_execute_at - Time.now, effort.id
-            end
-          end
+          ::Seam::Sidekiq.fire_the_worker_for_the_next_step_of effort
+        end
+      end
+    end
+
+    def self.fire_the_worker_for_the_next_step_of effort
+      next_worker = Seam::Worker.handler_for effort.next_step
+      if next_worker
+        if effort.next_execute_at <= Time.now
+          next_worker.class.perform_async effort.id
+        else
+          next_worker.class.perform_in effort.next_execute_at - Time.now, effort.id
         end
       end
     end
